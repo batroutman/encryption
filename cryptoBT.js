c6d0378351231f3aa7fbaba882526a97e773b3e9
batroutman <pencilboy20@sbcglobal.net>
2017-01-01 18:51:03 -0500

    Added new file: cryptoBT.js. Cleaner and more refined alogrithm.
    cryptoBT is the reboot of this project. It is a file that contains a
    JavaScript object which could be imported into any HTML file and used as
    an encryption library to encrypt and decrypt data using a
    programmer-defined key and cipher algorithm.
    
    The old files were experimental, and contained flaws in their execution
    and implementation. cryptoBT corrects these issues and essentially
    accomplishes the original goal: to create a basic encryption library for
    personal use on web pages.
    
    Obviously, the data encrypted using this library will not be hidden from
    capable malicious users, as JavaScript source is shown in web pages
    (though one may find this useful in a Node.js project), however, some
    may find this library useful for personal use, educational use, or to
    analyze the algorithm and port it to a sever-side language such as PHP,
    Perl, ASP.NET, etc. (which I might do in the future.)

---
+++ cryptoBT.js
@@ -0,0 +1,145 @@
+//cryptoBT.js
+//v1.0.0
+//Blake Troutman 2016
+
+function CryptoBT(givenKey="default key"){
+	
+	var myKey = givenKey;
+	var self = this;
+	
+	//getters and setters
+	this.getKey = function(){
+		return myKey;
+	}//end getKey
+	
+	this.setKey = function(text){
+		if(typeof(text) == "string"){
+			myKey = text;
+		}else{
+			console.log("ERROR: typeof " + text + " must be string.");
+		}//end branch
+	}//end setKey
+	
+	/* public methods */
+	
+	//fix mod
+	this.mod = function(n, m){
+		return ((n % m) + m) % m;
+	}//end mod
+	
+	//encryption function (return encrypted text)
+	this.encryptBT = function(text){ 
+		var key = myKey;
+		var original = text;
+		var outArray = [];
+		
+		//setup arrays
+		var keyArray = toIntArray(loopKey(key, text.length));
+		var originalArray = toIntArray(text);
+		
+		//combine arrays and bind to ASCII limits
+		outArray = addArrays(originalArray, keyArray);
+		outArray = bind(outArray);
+		
+		//cast the array of ints to a string
+		var result = this.arrayToString(outArray);
+		
+		return result;
+		
+	}//end encryptBT
+	
+	//decryption function (return decrypted text)
+	this.decryptBT = function(text){
+		var key = myKey;
+		var original = text;
+		var outArray = [];
+		
+		//setup arrays
+		var keyArray = toIntArray(loopKey(key, text.length));
+		var originalArray = toIntArray(text);
+		
+		//combine arrays and bind to ASCII limits
+		outArray = subArrays(originalArray, keyArray);
+		outArray = bind(outArray);
+		
+		//cast the array of ints to a string
+		var result = this.arrayToString(outArray);
+		
+		return result;
+		
+	}//end decryptBT
+	
+	//string -> Array[int]
+	var toIntArray = function(text){
+		var outArray = [];
+		
+		for(i = 0; i < text.length; i++){
+			outArray[i] = text.charCodeAt(i);
+		}//end for loop
+		
+		return outArray;
+	}//end toIntArray
+	
+	//create a string of a given length that loops through the given text
+	var loopKey = function(key, length){
+		var keyI = 0;
+		var result = "";
+		
+		for(i = 0; i < length; i++){
+			if(keyI >= key.length){
+				keyI = 0;
+			}
+			
+			result = result + key.charAt(keyI);
+			keyI = keyI + 1;
+		}
+		
+		return result;
+	}//end loopKey
+	
+	//output an array of the sum of two equally long int arrays
+	var addArrays = function(original, key){
+		var result = [];
+		
+		for(i = 0; i < original.length; i++){
+			result[i] = (original[i] + key[i]);
+		}//end for loop
+		
+		return result;
+	}//end addArrays
+	
+	//output an array of the difference of two equally long int arrays
+	var subArrays = function(original, key){
+		var result = [];
+		
+		for(i = 0; i < original.length; i++){
+			result[i] = (original[i] - key[i]);
+		}//end for loop
+		
+		return result;
+	}//end subArrays
+	
+	//converts array of integers to string of ASCII characters
+	this.arrayToString = function(arr){
+		var result = "";
+		
+		for(i = 0; i < arr.length; i++){
+			result = result + String.fromCharCode(arr[i]);
+		}//end for loop
+		
+		return result;
+	}//end arrayToString
+	
+	//limit the values of an int array to unicode limits
+	var bind = function(arr){
+		var result = [];
+		//-+ 32 mod 95
+		for(i = 0; i < arr.length; i++){
+			//result[i] = mod((arr[i] - 32) , 95) + 32;
+			result[i] = self.mod(arr[i], 281474976710656);
+		}//end for loop
+		
+		return result;
+	}//end bind
+	
+}
